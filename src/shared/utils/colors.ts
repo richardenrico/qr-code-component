0945a52e22b32619a66d8efaa216c202fd431c12
richardenrico <richardenrico02@gmail.com>
2023-12-15 20:10:42 +0800

    feat: add some color

---
+++ src/shared/utils/colors.ts
@@ -0,0 +1,8 @@
+export type Colors = 'white' | 'light_gray' | 'grayish_blue' | 'dark_blue';
+
+export const COLORS: Record<Colors, string> = {
+    white: '#FFFFFF',
+    light_gray: '#d6e2f0',
+    grayish_blue: '#7b879d',
+    dark_blue: '#1f3251',
+};
