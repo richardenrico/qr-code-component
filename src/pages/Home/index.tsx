c9a945b315d2d0852ee9fdb9914620d697d51dbf
richardenrico <richardenrico02@gmail.com>
2023-12-15 20:11:22 +0800

    feat: add QR Code Component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,40 @@
-import { Title } from "@mantine/core";
-import Seo from "shared/components/Seo";
+import { Container, Group, Image, Paper, Stack, Text } from '@mantine/core';
+import Seo from 'shared/components/Seo';
+import QRImage from 'assets/images/image-qr-code.png';
 
-const Home = () => {
-    return (
-        <>
-            <Seo title={"Hello World"} />
-            <Title>Hello World</Title>
-        </>
-    );
-};
+const Home = () => (
+    <>
+        <Seo title={'QR Code Component'} />
+        <Container fluid bg={'light_gray'} h={'100vh'}>
+            <Group justify="center" h={'100%'}>
+                <Stack align="center">
+                    <Paper h={450} w={280} shadow="xl" radius="lg" p="md">
+                        <Image radius="md" src={QRImage} />
+                        <Stack mt={'md'} gap="xs">
+                            <Text
+                                fz={'xl'}
+                                fw={700}
+                                ta="center"
+                                c={'dark_blue'}
+                            >
+                                Improve your front-end skills by building
+                                projects
+                            </Text>
+                            <Text
+                                fz={'sm'}
+                                fw={400}
+                                ta="center"
+                                c={'grayish_blue'}
+                            >
+                                Scan the QR code to visit Frontend Mentor and
+                                take your coding skills to the next level
+                            </Text>
+                        </Stack>
+                    </Paper>
+                </Stack>
+            </Group>
+        </Container>
+    </>
+);
 
 export default Home;
